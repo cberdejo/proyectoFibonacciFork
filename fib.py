299a064fff5028b1dd38b925416aafe0e18349ad
Marina Parra <marinaparrag8@uma.es>
2024-11-07 20:41:36 +0100

    all changes

---
+++ fib.py
@@ -0,0 +1,55 @@
+import argparse
+
+
+def fibonacci_iterative(n: int) -> int:
+    """
+    Computes the n-th Fibonacci number
+    :param n: i-th Fibonacci number
+    :return: The n-th fibonacci number
+    """
+    if n < 0:
+        raise ValueError("n must be greater than or equal to zero.")
+    if n < 2:
+        return n
+
+    n0 = 0
+    n1 = 1
+    nth = 0
+
+    for _ in range(n - 1):
+        nth = n0 + n1
+        n0 = n1
+        n1 = nth
+    return nth
+
+
+def fibonacci_recursive(n: int) -> int:
+    """
+    Computes the n-th Fibonacci number
+    :param n: i-th Fibonacci number
+    :return: The n-th fibonacci number
+    """
+    if n < 0:
+        raise ValueError("n must be greater than or equal to zero.")
+    if n < 2:
+        return n
+    n0 = 0
+    n1 = 1
+    for _ in range(n):
+        nth = n0 + n1
+        n0 = n1n1 = nth
+    return n0
+
+
+cache = {}
+
+# return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('nth', type=int, help="Nth fibonacci number.")  # positional argument
+    args = parser.parse_args()
+
+    result = fibonacci_iterative(args.nth)
+    print(result)
